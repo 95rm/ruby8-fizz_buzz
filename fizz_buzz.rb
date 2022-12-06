a43acd14e89a2c3d185fc748ab568fe7ac5bb8ae
95rm <119680568+95rm@users.noreply.github.com>
2022-12-06 13:26:21 +0000

    [Add1-fizz_buzz]

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,5 +1,15 @@
 def fizz_buzz(number)
   
+  if number % 3==0
+    puts "Fizz"
+  elsif number % 5==0
+    puts "Buzz"
+  elsif (number % 3==0) && (number % 5==0)
+    puts "FizzBuzz"
+  else (number% 3==1) || (number % 5==1)
+    fizz_buzz(input)
+  end
+  
 end
 
 puts "数字を入力してください"
